e46a11221b133278ac4287ef24b52e219d454f5b
Addin15 <addin.utb15@gmail.com>
2022-04-04 09:53:30 +0800

    add table

--- database/migrations/2022_04_04_004923_create_companies_table.php
+++ database/migrations/2022_04_04_004923_create_companies_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone');
+            $table->string('fax');
             $table->string('address');
             $table->string('about');
             $table->string('email');
