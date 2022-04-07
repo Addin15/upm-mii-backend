a350046fe614ccd770611667b71a649f0dcba4be
Addin15 <addin.utb15@gmail.com>
2022-04-07 16:39:07 +0800

    add lagi

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class UserProfile extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'nric',
+        'gender',
+        'birth_date',
+        'age',
+        'phone',
+        'state',
+        'address',
+    ];
 }
